8fc2dd0864798087d7d452ddb92701e6e7591af8
tyZulh <thomas.paoli.tp@gmail.com>
2021-07-05 15:09:43 +0200

    display contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return <div className="App">Wild P3</div>;
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(process.env.REACT_APP_API_BASE_URL);
+      setContacts(res.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Wild P3</h1>
+      {contacts.map((contact) => {
+        return (
+          <div>
+            <span>{contact.first_name}</span>
+            <span>{contact.last_name}</span>
+            <span>{contact.email}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
