0ff57cbb2e2371dbe6ba9e7dde3ec2131777f380
tyZulh <thomas.paoli.tp@gmail.com>
2021-07-05 15:15:11 +0200

    bugfix : display contacts

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log(process.env.REACT_APP_API_BASE_URL);
       const res = await axios.get(process.env.REACT_APP_API_BASE_URL);
       setContacts(res.data);
     };
